bafe6dbb8212708bd0eddfef58e13aa7c2962f91
lucas <lucas.lr184@gmail.com>
2022-04-22 22:30:41 -0300

    envio de informacoes por forms

--- TemplateEngine/public/app.js
+++ TemplateEngine/public/app.js
@@ -1,17 +1,24 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 
 var app = express();
+var urlEncodedParser = bodyParser.urlencoded({ extended:false });
 
 app.set('view engine', 'ejs');
-
 app.use('/assets', express.static('../assets'));
+
 
 app.get('/', function(req, res){
     res.render('partials/index');
 });
 
 app.get('/contact', function(req, res){
-    res.render('partials/contact');
+    res.render('partials/contact', {qs: req.query});
+});
+
+app.post('/contact-success', urlEncodedParser, function(req, res){
+    console.log(req.body);
+    res.render('partials/contact-success', {data: req.body});
 });
 
 app.get('/profile/:name', function (req, res){
@@ -29,4 +36,4 @@ app.get('/profile/:name', function (req, res){
     res.render('profile', {person: req.params.name, data: data});
 });
 
-app.listen(3000);
+app.listen(5000);
